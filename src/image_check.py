1f4841b1a93b609a6e935cc878994f9bf34b2f33
Urmzd <urmzds@gmail.com>
2022-03-24 20:38:18 -0400

    Update dependencies for LSP support.

--- src/image_check.py
+++ src/image_check.py
@@ -9,66 +9,17 @@ Script Results:
 
 '''
 
-from os import name
-from typing import Optional
 from difPy import dif
-import requests
-import pathlib
-
-
-import pandas as pd
-import requests
-import re
-import pathlib
+from data_retrieval import get_data, download_image
 
 DATA_PATH="resources/data.csv"
 IMAGE_PATH="/tmp/images"
 
-def get_data() -> pd.DataFrame:
-  df = pd.read_csv(DATA_PATH, usecols=range(2))
-  if isinstance(df, pd.DataFrame):
-      return df
-
-  raise Exception("THIS SHOULD NEVER HAPPEN.")
-
-def download_image(link: str) -> pathlib.Path:
-  # view  : https://regex101.com/r/3bhDMM/1
-  # delete: https://regex101.com/delete/N5sItwbrPF73ZllTnRDltxZ1
-  file_name_regex = re.compile(r".*\/(.*(\.(jpeg|jpg|png))?)\??.*", flags=re.IGNORECASE)
-  regex_matches = file_name_regex.match(link)
-
-  if not regex_matches:
-    raise Exception(f"Failed to match file_name for link {link}")
-
-  if len(regex_matches.groups()) < 3:
-    file_name = regex_matches.group(1) + ".png"
-  else:
-    file_name = regex_matches.group(1)
-
-  content_path = pathlib.Path(IMAGE_PATH)
-  content_path.mkdir(parents=True, exist_ok=True)
-
-  file_path = content_path / file_name
-
-  if file_path.exists():
-    return file_path
-
-  image_request_headers={
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
-  }
-
-  image = requests.get(link, headers=image_request_headers)
-
-  with open(file_path, "wb") as handle:
-    handle.write(image.content)
-
-  return file_path
-
-if name == "__main__":
-    df = get_data()
+if __name__ == "__main__":
+    df = get_data(DATA_PATH)
 
     for link in df.iloc[:, 1].tolist():
-        download_image(link)
+        download_image(link, IMAGE_PATH)
 
     search = dif("/tmp/images")
 
