49622c7aa4d1777c7217c1608a2d2a2628d426c1
Urmzd <urmzds@gmail.com>
2022-03-30 20:01:44 -0400

    Simplify bootstrap process.

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -3,7 +3,6 @@ import sys
 from pytorch_lightning import seed_everything, Trainer
 from pathlib import Path
 from loguru import logger
-import wandb
 
 from src.data.data_processing import get_image_encoder
 from src.data.data_handler import (
@@ -14,32 +13,45 @@ from src.data.data_handler import (
 
 from pytorch_lightning.loggers import WandbLogger
 
-seed_everything(42)
+LOG_LEVEL = "INFO"
+DATA_MANFIEST_PATH = Path("./resources/data.csv")
+IMAGE_FOLDER_PATH = Path("/tmp/images")
+HEIGHT = 200
+WIDTH = 200
+SCALE_HEIGHT = False
+BATCH_SIZE = 2
+NUM_FOLDS = 5
+EXPORT_PATH = Path("model_checkpoints")
 
-if __name__ == "__main__":
-    logger.remove()
-    LOG_LEVEL = "INFO"
-    logger.add(sys.stderr, level=LOG_LEVEL)
+
+def bootstrap(
+    model=SampleModel(),
+    log_level=LOG_LEVEL,
+    data_manfiest_path=DATA_MANFIEST_PATH,
+    image_folder_path=IMAGE_FOLDER_PATH,
+    height=HEIGHT,
+    width=WIDTH,
+    scale_height=SCALE_HEIGHT,
+    batch_size=BATCH_SIZE,
+    num_folds=NUM_FOLDS,
+    export_path=EXPORT_PATH
+):
+    export_path.mkdir(exist_ok=True, parents=True)
     seed_everything(42)
-    DATA_MANFIEST_PATH = Path("./resources/data.csv")
-    IMAGE_FOLDER_PATH = Path("/tmp/images")
-    HEIGHT = 200
-    WIDTH = 200
-    SCALE_HEIGHT = False
-    BATCH_SIZE = 2
-    NUM_FOLDS = 5
-    model = SampleModel()
+    logger.remove()
+    logger.add(sys.stderr, level=log_level)
     x_encoder = partial(
         get_image_encoder(
-            desired_height=HEIGHT, desired_width=WIDTH, scale_height=SCALE_HEIGHT
+            desired_height=height, desired_width=width, scale_height=scale_height
         )
     )
+
     datamodule = LepusStratifiedKFoldDataModule(
-        data_manifest_path=DATA_MANFIEST_PATH,
-        image_folder_path=IMAGE_FOLDER_PATH,
+        data_manifest_path=data_manfiest_path,
+        image_folder_path=image_folder_path,
         transform_features=x_encoder,
-        batch_size=BATCH_SIZE,
-        n_splits=NUM_FOLDS,
+        batch_size=batch_size,
+        n_splits=num_folds,
     )
 
     wandb_logger = WandbLogger()
@@ -56,10 +68,8 @@ if __name__ == "__main__":
         logger=wandb_logger,
     )
 
-    EXPORT_PATH = Path("model_checkpoints")
-    EXPORT_PATH.mkdir(exist_ok=True, parents=True)
 
     internal_fit_loop = trainer.fit_loop
-    trainer.fit_loop = StratifiedKFoldLoop(NUM_FOLDS, export_path=EXPORT_PATH)
+    trainer.fit_loop = StratifiedKFoldLoop(num_folds, export_path=export_path)
     trainer.fit_loop.connect(internal_fit_loop)
     trainer.fit(model, datamodule)
