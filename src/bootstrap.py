7c1b34740bbc745b08baa693c07908c10873b322
Urmzd <urmzds@gmail.com>
2022-04-10 18:25:51 -0400

    Fix syntax mistakes.

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -41,7 +41,7 @@ def get_default_callbacks() -> List[Callback]:
 class TrainerFactory:
     logger: Optional[WandbLogger] = None
     callbacks: List[Callback] = field(default_factory=get_default_callbacks)
-    strategy: Optional[Union[str, strategies.Strategy]] = "dp",
+    strategy: Optional[Union[str, strategies.Strategy]] = "dp"
     max_epochs: int = 10
     devices: Union[List[int], str, None] = "auto"
     deterministic: bool = True
